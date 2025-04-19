ac723d8464bf55dabb5f177b4a52571dd5158bbd
Stefano Celso <me@stefanocelso.com>
2025-04-19 10:50:15 +0200

    Stack update

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
-    'version' => '0.9.0',
+    'version' => '1.0.0',
 
     /*
     |--------------------------------------------------------------------------
